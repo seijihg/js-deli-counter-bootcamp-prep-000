025491cd7a76bcf6e8ad6b9b5c11c5a441ac5bee
Le H Ngo <seiji@o2.pl>
2019-05-13 07:39:21 +0000

    Automatically backed up by Learn

--- tlearn.js
+++ tlearn.js
@@ -37,9 +37,18 @@ takeANumber(katzDeliLine, "Pawel")
 
 var line = ["A", "B", "C", "D", "E"]
 
+<<<<<<< HEAD
 var cars = ["BMW", "Volvo", "Saab", "Ford", "Fiat", "Audi"];
 var text = "";
 for (i = 0; i < cars.length; i++) {
   text += i + ". " + cars[i] + " ";
 }
 console.log(text.substring(0, text.length - 2))
+=======
+var str = "This is an amazing sentence.";
+
+var str1 = line.toString()
+
+var words = str1.split(" ");
+console.log(words);
+>>>>>>> 863e3bc1b84e3c4721212bbcbefe99f2ee3102db
